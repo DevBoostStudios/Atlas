a6ab370efcf74b6c8405667191b6ba1293044e4f
Ethan Chrisp <EthanChrisp@users.noreply.github.com>
2017-08-08 01:38:23 -0500

    Prep for further CallofDuty commands

--- src/Atlas/Modules/Administration/Management.cs
+++ src/Atlas/Modules/Administration/Management.cs
@@ -4,7 +4,6 @@ using Discord.Commands;
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Atlas.Modules.Administration
--- src/Atlas/Modules/Utility/CallofDuty.cs
+++ src/Atlas/Modules/Utility/CallofDuty.cs
@@ -1,8 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
+using HtmlAgilityPack;
 using System;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 
 namespace Atlas.Modules.Utility
 {
@@ -13,13 +13,10 @@ namespace Atlas.Modules.Utility
         public class IW : ModuleBase<SocketCommandContext>
         {
             [Command("stats")]
-            [Summary("Retrieve multiplayer statistics for Call of Duty: Infinite Warfare players.")]
+            [Summary("Retrieve multiplayer statistics for the specified Call of Duty: Infinite Warfare player.")]
             public async Task IWStats(string platform, string username)
             {
-                // Set Profile URL
                 string url = "https://my.callofduty.com/iw/stats?platform=" + platform + "&username=" + username;
-
-                // Fetch Profile HTML
                 HtmlDocument htmlDoc = new HtmlWeb().Load(url);
 
                 // Get Prestige Value
@@ -45,28 +42,46 @@ namespace Atlas.Modules.Utility
                         .WithName(username + " (" + platform + ")")
                         .WithIconUrl("https://my.callofduty.com/content/dam/atvi/callofduty/mycod/common/player-icons/iw/prestige-" + PrestigeValue + ".png");
                     })
-                    .WithColor(new Color(5025616))
-                    .AddInlineField("Prestige", PrestigeValue)
-                    .AddInlineField("Rank", "55")
-                    .AddInlineField("Played", "12 Days")
-                    .AddInlineField("Kills", "123,456")
-                    .AddInlineField("Deaths", "789,123")
-                    .AddInlineField("Ratio", "1.23")
-                    .AddInlineField("Wins", "123")
-                    .AddInlineField("Losses", "456")
-                    .AddInlineField("Ratio", "1.23")
-                    .AddInlineField("Score", "1,234,567")
-                    .AddInlineField("SPM", "123")
-                    .AddInlineField("Matches", "1234")
-                    .WithFooter(footer =>
-                    {
-                        footer
-                        .WithText(Context.User.ToString() + " | " + DateTime.Now.ToString())
-                        .WithIconUrl(Context.User.GetAvatarUrl());
-                    });
+                .WithColor(new Color(5025616))
+                .AddInlineField("Prestige", PrestigeValue)
+                .AddInlineField("Rank", "55")
+                .AddInlineField("Played", "12 Days")
+                .AddInlineField("Kills", "123,456")
+                .AddInlineField("Deaths", "789,123")
+                .AddInlineField("Ratio", "1.23")
+                .AddInlineField("Wins", "123")
+                .AddInlineField("Losses", "456")
+                .AddInlineField("Ratio", "1.23")
+                .AddInlineField("Score", "1,234,567")
+                .AddInlineField("SPM", "123")
+                .AddInlineField("Matches", "1234")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText(Context.User.ToString() + " | " + DateTime.Now.ToString())
+                    .WithIconUrl(Context.User.GetAvatarUrl());
+                });
                 var embed = builder.Build();
                 await ReplyAsync("", false, embed)
                     .ConfigureAwait(false);
+            }
+
+            [Command("leaderboard")]
+            [Summary("Return Leaderboard statistics for the specified Leaderboard position.")]
+            public async Task IWLeaderboard(string position, string platform)
+            {
+                var url = "https://my.callofduty.com/iw/leaderboard?platform=" + platform;
+                HtmlDocument htmlDoc = new HtmlWeb().Load(url);
+            }
+
+            [Command("status")]
+            [Summary("Return the current server status of Call of Duty: Infinite Warfare.")]
+            public async Task IWStatus()
+            {
+                string url = "https://support.activision.com/onlineservices?gameTitle=Call+of+Duty%3A+Infinite+Warfare";
+                HtmlDocument htmlDoc = new HtmlWeb().Load(url);
+
+
             }
         }
     }
