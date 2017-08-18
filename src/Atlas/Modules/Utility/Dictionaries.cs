61f7d97ada4212b50dd981912aafd336c79c176e
Ethan Chrisp <EthanChrisp@users.noreply.github.com>
2017-08-17 23:09:03 -0500

    Rename to Dictionaries, Complete Hashtag Command

--- src/Atlas/Modules/Utility/Dictionaries.cs
+++ src/Atlas/Modules/Utility/Dictionaries.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Atlas.Modules.Utility
 {
-    public class UrbanDictionary : ModuleBase<SocketCommandContext>
+    public class Dictionaries : ModuleBase<SocketCommandContext>
     {
         [Command("urban")]
         [Summary("Returns the top result for the specified word from Urban Dictionary.")]
@@ -37,6 +37,45 @@ namespace Atlas.Modules.Utility
                     .AddInlineField("Thumbs Up", upvotes)
                     .AddInlineField("Thumbs Down", downvotes)
                     .AddField("Example", example)
+                    .WithFooter(footer =>
+                    {
+                        footer
+                        .WithText(Context.User.ToString() + " | " + DateTime.Now.ToString())
+                        .WithIconUrl(Context.User.GetAvatarUrl());
+                    });
+                var embed = builder.Build();
+                await ReplyAsync("", false, embed)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        [Command("hashtag")]
+        [Summary("Returns the top result for the specified hashtag from TagDef.")]
+        public async Task Hashtag(string hashtag)
+        {
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync("https://api.tagdef.com/one." + hashtag + ".json");
+                dynamic parse = JsonConvert.DeserializeObject(json);
+
+                string definition = parse.defs.def.text;
+                string created = parse.defs.def.time;
+                string upvotes = parse.defs.def.upvotes;
+                string downvotes = parse.defs.def.downvotes;
+
+                var builder = new EmbedBuilder()
+                    .WithColor(new Color(5025616))
+                    .WithAuthor(author =>
+                    {
+                        author
+                        .WithName("TagDef - #" + hashtag.Substring(0, 1).ToUpper() + hashtag.Substring(1))
+                        .WithIconUrl("http://i.imgur.com/odf04Fm.jpg");
+                    })
+                    .WithUrl("https://tagdef.com/en/tag/" + hashtag)
+                    .WithDescription(definition)
+                    .AddInlineField("Thumbs Up", upvotes)
+                    .AddInlineField("Thumbs Down", downvotes)
+                    .AddInlineField("Created", created)
                     .WithFooter(footer =>
                     {
                         footer
